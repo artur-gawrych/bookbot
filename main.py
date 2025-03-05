0ae32c0bb0585e8803c6e4beb8c8454a6adea918
Artur <artur.gawrych@gmail.com>
2025-03-05 21:53:22 +0100

    added argv as book path

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ get_number_of_words,
 get_char_count,
 get_sorted_chars
 )
+from sys import argv
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -21,10 +22,13 @@ def print_report(book, num_words, sorted_chars):
     print("============= END ===============")
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    if len(argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+    bookpath = argv[1]
+    text = get_book_text(bookpath)
     num_words = get_number_of_words(text)
     sorted_chars = get_sorted_chars(get_char_count(text))
-    print_report(book_path, num_words, sorted_chars)
+    print_report(bookpath, num_words, sorted_chars)
 
 main()
